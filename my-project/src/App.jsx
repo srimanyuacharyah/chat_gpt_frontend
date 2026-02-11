341113e098d972ee6504c23ab20c73b3dffe1cc6
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-02-11 12:04:14 +0530

    commits

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import Header from './components/Header'
 import Footer from './components/footer'
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Header />
-      
+
 
       <Routes>
         <Route path="/" element={<Home />} />
