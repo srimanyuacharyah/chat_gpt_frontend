773f498ee3c9a82a03dc2b4fa990c36cb92abcad
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-02-11 12:07:47 +0530

    f

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-        
+     <h1>Header</h1>   
       <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
         <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
         <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
