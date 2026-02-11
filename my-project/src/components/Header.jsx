c1820637a135bb009a0b3d09cd5e0bf4839b4ad4
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-02-11 11:56:04 +0530

    comit

---
+++ my-project/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+      <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
+        <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
+        <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
+        <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
